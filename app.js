01583281820224e588e2e0225ef01bfe3f790145
Lisa Pan <l.pan4952@gmail.com>
2016-11-11 17:07:44 -0500

    initial set up

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+
+var env = nunjucks.configure('views', {noCache: true});
+
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
+
