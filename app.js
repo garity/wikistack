626ca839de91d81498d29b004f19353b70d4ed93
Lisa Pan <l.pan4952@gmail.com>
2016-11-11 17:29:43 -0500

    onto schemas

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 
